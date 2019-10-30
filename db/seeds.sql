d457d4e538915158a18e6ca0a92b3e2b8b620fee
jjestrella <estrella.jjuan@gmail.com>
2019-10-29 22:49:34 -0700

    db created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken Club', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pastrami Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', true);
